c29bab19fac6ed92d1eec8c15a0aefce8e41c159
Dennis <dmyasnyankin@berkeley.edu>
2018-11-07 20:59:16 -0800

    finish warm up

--- W3D4/Active_Record_WarmUp/movie_novice/queries.rb
+++ W3D4/Active_Record_WarmUp/movie_novice/queries.rb
@@ -40,7 +40,7 @@ end
 def star_wars
   #display the id, title and year of each Star Wars movie in movies.
   # hint: use 'select' and 'where'
-  Movie.select(:id, :title, :yr).where('title LIKE \'%Star Wars%\'')
+  Movie.select('id', 'title', 'yr').where('title LIKE \'%Star Wars%\'')
 end
 
 
@@ -49,6 +49,7 @@ def below_average_years
   #with the count of movies scoring under 5 aliased as bad_movies,
   #in descending order
   # hint: use 'select', 'where', 'group', 'order'
+  Movie.select('yr', 'COUNT(*) as bad_movies').where('score < 5').group('yr').order('bad_movies DESC')
 
 end
 
@@ -58,14 +59,16 @@ def alphabetized_actors
   # Note: Ubuntu users may find that special characters
   # are alphabetized differently than the specs.
   # This spec might fail for Ubuntu users. It's ok!
-
+  Actor.order('name ASC').limit(10)
 end
 
 def pulp_fiction_actors
   # practice using joins
   # display the id and name of all actors in the movie Pulp Fiction
   # hint: use 'select', 'joins', 'where'
-
+  Actor.select(:name, :id)
+        .joins(:movies)
+        .where('title = \'Pulp Fiction\'')
 end
 
 def uma_movies
@@ -73,5 +76,6 @@ def uma_movies
   # display the id, title, and year of movies Uma Thurman has acted in
   # order them by ascending year
   # hint: use 'select', 'joins', 'where', and 'order'
-
+  Movie.select(:id, :title, :yr).joins(:actors)
+       .where('name = \'Uma Thurman\'').order('yr ASC')
 end
